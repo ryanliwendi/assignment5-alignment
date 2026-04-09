47ccb8f20ed526f8088a8b563e2e16c32b6f96a0
ryanliwendi <liryan@usc.edu>
2026-04-08 21:37:53 -0700

    Implement sft_microbatch_train_step

--- cs336_alignment/sft_utils.py
+++ cs336_alignment/sft_utils.py
@@ -87,7 +87,7 @@ def sft_microbatch_train_step(
     gradient_accumulation_steps: int,
     normalize_constant: float = 1.0,
 ) -> tuple[torch.Tensor, dict[str, torch.Tensor]]:
-    loss = -masked_normalize(policy_log_probs, response_mask, normalize_constant)
+    loss = -0.5 * masked_normalize(policy_log_probs, response_mask, normalize_constant) / gradient_accumulation_steps
     loss.backward()
     metadata = {}
     return loss, metadata
